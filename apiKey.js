1b45aaab4d337766ca9583381bdd9db1e5fc9087
shadow2600 <joshuaperez0420@gmail.com>
2019-11-22 00:30:51 -0500

    Delete apiKey.js

--- apiKey.js
+++
@@ -1,9 +0,0 @@
-const apiKey = process.key.GIPHY_API_TOKEN || require('./key').default;
-
-function getAPI (searchTerm) {
-    return `http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=${apiKey}&limit=5`;
-}
-
-module.exports = {
-    getAPI
-}
